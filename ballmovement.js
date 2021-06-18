c24a769d90a0b7f5a543402937f3697fa7bc5b16
amyvoss <85448994+amyvoss@users.noreply.github.com>
2021-06-18 01:54:19 -0400

    Add files via upload

---
+++ ballmovement.js
@@ -0,0 +1,29 @@
+  var Xmin = 0;
+  var Xmax = 300;
+
+  var Ymin = 0;
+  var Ymax = 300;
+
+  if (reverse) {
+    positionX = positionX - velocity;
+    positionY = positionY - velocity;
+    ball.style.left = positionX + "px";
+    ball.style.top = positionY + "px";
+  } else {
+    positionX = positionX + velocity;
+    positionY = positionY + velocity;
+    ball.style.left = positionX + "px";
+    ball.style.top = positionY + "px";
+  }
+
+  if (
+    positionX > Xmax ||
+    positionX === Xmin ||
+    positionY > Ymax ||
+    positionY === Ymin
+  ) {
+    reverse = !reverse;
+  }
+}
+
+setInterval(moveBall, 100);
